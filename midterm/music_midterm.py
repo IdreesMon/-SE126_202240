a4bb8f5da255060cfabe1946bbc8ed2b259f2f4e
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-02-13 21:53:33 -0500

    midterm almost

--- midterm/music_midterm.py
+++ midterm/music_midterm.py
@@ -4,7 +4,7 @@
 #02/12/2024
 
 #prompt:
-#This program will prompt user with a menu function from which they can select between a music genre(1), a curated mood playlist(2) &  (3) based on user input from prompted questions,  or to exit the program(4). The program will then display the user's selection and then display the playlist of the user's selection. The program will then prompt the user to select another option from the menu. The program will continue to loop until the user selects the exit option.
+#This program will prompt user with a menu function from which they can select between a music genre(1), a curated mood playlist(2) &  (3) dislay list of entire playlist and total records,  or to exit the program(4). The program will then display the user's selection and then display the playlist of the user's selection. The program will then prompt the user to select another option from the menu. The program will continue to loop until the user selects the exit option.
 
 #variable dictionary:
 
@@ -21,9 +21,6 @@ import csv
 
 #create some lists first
 genre = []
-rock = []
-reggae = []
-hiphop = []
 year = []
 song =[]
 artist = []
@@ -46,6 +43,7 @@ with open("midterm/music.csv") as libra:
 rock = []
 reggae = []
 hiphop = []
+rnb = []
 
 
 
@@ -87,7 +85,8 @@ if menu_choice== 1:
     print("1. Rock")
     print("2. Hip Hop")
     print("3. Reggae")
-    print("4. Exit")
+    print("4. RnB")
+    print("5. Exit")
     print("\n")
     genre_choice = (input("Please select your preffered genre: "))
 
@@ -111,25 +110,54 @@ if menu_choice== 1:
             if genre[i] == "Reggae":
                 print(f"{genre[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
 
+    elif genre_choice == "4":
+        for i in range(0, len(genre)):
+            if genre[i] == "RnB":
+                print(f"{genre[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
+
+       
+
 
 #--------------------- CHOICE 2 ---------------------
-        
+mood =[]
+upbeat = []
+melancholy = []
+motivated = []
+relaxed = []     
+
 if menu_choice == 2:
-    mood =[]
-    upbeat = []
-    melancholy = []
-    motivated = []
-    relaxed = []
-    
     print("\n")
     print("1. Upbeat")
     print("2. Melancholy")
     print("3. Angry")
     print("4. Relaxed")
-    mood = ("Select your mood: ")
+    mood_choice = (input("\nSelect your mood: "))
     
-    print(f"\n{'UPBEAT':8} {'MELANCHOLY':8} {'MOTIVATED':6} {'RELAXED':6}")
-    print("-------------------------------------------------")
+    #print(f"\n{'UPBEAT':8} {'MELANCHOLY':8} {'MOTIVATED':6} {'RELAXED':6}")
+    #print("-------------------------------------------------")
+
+    if mood_choice == "1":
+        for i in range(0, len(mood)):
+            if mood[i] == "Upbeat":
+                print(f"\n{'UPBEAT':8}")
+                print(f"{mood[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
+    elif mood_choice == "2":
+        for i in range(0, len(mood)):
+            if mood[i] == "Melancholy":
+                print(f"{mood[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
+    elif mood_choice == "3":
+        for i in range(0, len(mood)):
+            if mood[i] == "Motivated":
+                print(f"{mood[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
+    elif mood_choice == "4":
+        for i in range(0, len(mood)):
+            if mood[i] == "Relaxed":
+                print(f"{mood[i]:7} {year[i]:8} \t {artist[i]:15} \t{song[i]:8}")
+                
+                
+    
+   
+   
 elif menu_choice == 3: #while menu_choice is not the exit option 
 
     print(f"\n{'GENRE':12} \t {'YEAR':12} \t {'ARTIST'} \t\t\t {'SONG':15}")
